df6a6abaa2357dac2109d6216b3acd41ec2fb167
zssy12 <1269297656@qq.com>
2020-07-27 18:18:31 +0800

    update

---
+++ src/pages/index/index.vue
@@ -0,0 +1,230 @@
+<template>
+	<view class="content">
+		<image src="../../static/img/bg1.png" style="height: 100%;width: 100%;"></image>
+		<view class="title">
+			<image src="../../static/img/logo.png"></image>
+		</view>
+		<view class="information">
+			<image src="../../static/img/border.png"></image>
+			<view class="info">
+				<view class="p_infor" style="margin-left: 35px;">
+					<picker @change="bindAreaChange" :value="index" :range="array" range-key="name">
+						 大区<view class="uni-input" >{{array[index].name}}
+							 <image src="../../static/img/JT.png" ></image>
+						 </view>
+					</picker>
+				</view>
+				<view class="p_infor">
+					<picker @change="bindStrageChange" :value="Stageindex" :range="arrayStage" range-key="name">
+						 当前段位<view class="uni-input" style="width:55%">{{arrayStage[Stageindex].name}}
+							 <image src="../../static/img/JT.png" ></image>
+						 </view>
+					    
+					</picker>
+				</view>
+				<view class="p_infor">
+				角色名称 <input class="in-input" type="text" v-model="role" placeholder="请输入你的角色名称"/>
+				</view>
+				<view class="p_infor">
+					陪练盘数 <input class="in-input" type="text" placeholder="请输入陪练盘数"/>
+				</view>
+				<view class="p_infor" style="margin-left: 15px;">
+					<span style="position: relative;bottom:23px">陪练员</span>
+					 <checkbox-group class="checkbox-group" @change="checkboxChange">
+					     <label  v-for="item in items" :key="item.value" style="font-size:15px">
+								<checkbox :value="item.value" :checked="item.checked" />{{item.name}} <br>
+					     </label>
+					</checkbox-group><br>
+					<input class="in-input" type="text" placeholder="请输入陪练编号" style="width: 60%;left: 25%;"/>
+				</view>
+				<view class="p_infor">
+					联系方式<input class="in-input" type="text" placeholder="请输入你的角色名称"/>
+				</view>
+				<!-- 按钮 -->
+				<view>
+					<button class="b_next_button" @click="openPrompt">下一步</button>
+					<uni-popup ref="popupt" type="center">
+						<view class="nui-tip1" style="position:relative;margin:0">
+							<view style="background-color:white;opacity:1 ;width: 230px;height: 190px;" class="popup-v">
+								<view>
+									<image src="../../static/img/prompt.png" style="width: 230px;height: 70px;position: relative;left: 50%;margin-left: -115px"></image>
+								</view>
+								<view >
+									<h6 style="position: relative;left: 15px;">营业时间</h6><br>
+									<view style="position:relative;top:5px">
+										<image src="../../static/img/time.png" style="width:15px;height:15px;margin:0;padding:0;position: relative;left: 15px;top: 2px;"></image>
+										<text style="position:relative;left:20px;font-size:15px">9:00-23:00</text><br>
+										<view style="width:195px;position: relative;left: 15px;">
+											<text style="color:#C8C8C8;font-size:10px">如不在工作时间段下单，订单接单可能稍有延迟， 请耐心等待至工作时间</text>
+										</view>
+									</view>
+								</view>
+							</view>
+							<!-- 页面跳转 -->
+							<navigator url="/pages/index/orderConfirm?title=navigate" hover-class="navigator-hover">
+								<image class="cancel-p" src="../../static/img/cancel.png" style="width:20px;height:20px"></image>
+							</navigator>
+							
+						</view>
+					</uni-popup>
+				</view>
+			</view>
+		</view>
+	</view>
+	
+</template>
+
+<script>
+	import uniPopup from '@dcloudio/uni-ui/lib/uni-popup/uni-popup'
+	export default {
+		components:{
+			uniPopup
+		},
+		data() {
+			return {
+				title: '英雄联盟',
+				titleLog:'LEAGUE  LEGENDS',
+				role:'',
+				picked:[],
+				items:[
+					{name:'随机陪练员'},
+					{name:'指定陪练员'}
+				],
+				array: [{name:'艾欧尼亚 '},{name: '祖安'}, {name:'诺克萨斯'}, {name:'班德尔城'}],
+				index: 0,	
+				arrayStage: [{name:'青铜'},{name:'白银'},{name:'黄金'},{name:'铂金'},{name:'钻石'},{name:'大师'},{name:'王者'}],
+				Stageindex:0,
+				showtip:false,
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+			checkboxChange: function (e) {
+			    var items = this.items,
+			    values = e.detail.value;
+			    for (var i = 0, lenI = items.length; i < lenI; ++i) {
+			    const item = items[i]
+			        if(values.includes(item.value)){
+			            this.$set(item,'checked',true)
+			        }else{
+			            this.$set(item,'checked',false)
+			         }
+			    }
+			},
+			bindAreaChange: function(e) {
+				this.index = e.detail.value
+			},
+			bindStrageChange: function(e) {
+				this.Stageindex = e.detail.value
+			},
+			// 弹框
+			openPrompt() {
+				this.$refs.popupt.open()
+			},
+		}
+	}
+</script>
+
+<style>
+	page{
+		height: 100%;
+	}
+	.content {
+		position: relative;
+		background-color: #000048;
+		width: 100%;
+		height: 100%;
+		
+	}
+	.title{
+		position: absolute;
+		top:5%;
+		left: 50%;
+		margin: 0 0 0 -80px;
+	}
+	.title image{
+		width: 167px;
+		height: 60px;
+	}
+	.information{
+		width:100%;
+		height: 75%;
+		position: absolute;
+		top:17%;
+	}
+	.information image{
+		width:80%;
+		height: 105%;
+		position: absolute;
+		left: 50%;
+		margin: 0 0 0 -40%;
+	}
+	.info{
+		position: absolute;
+		left: 15%;
+		top:5%;
+		/* width: 100%; */
+	}
+	.p_infor{
+		color: #FAD154;
+		font-size: 17px;
+		margin-top: 10px;
+	}
+	.uni-input{
+		display:inline-block;
+		color:white;
+		width: 65%;
+		position: relative;
+		left: 10px;
+		font-size: 15px;
+		border-bottom:1px solid #FFFFFF
+	}
+	.uni-input image{
+		width:13px;
+		height:8px;
+		left: 130%; 
+		top:10px;
+	}
+	.in-input{
+		display: inline-block;
+		position: relative;
+		top:5px;
+		left: 10px;
+		border-bottom: 1px solid #FFFFFF;
+		width: 55%;
+		color: #FFFFFF;
+		font-size: 15px;
+	}
+	checkbox .wx-checkbox-input{
+		width: 20px;
+		height: 20px;
+		border-radius: 5px;
+		background-color: inherit;
+	} 
+	.checkbox-group{
+		display: inline-block;
+		margin-top: 5px;
+		margin-left: 12px;
+		color: #FFFFFF;
+	}
+	.b_next_button{
+		background-color: #346CD5;
+		height: 30px;
+		line-height: 1.7;
+		color: #fff;
+		width: 80%;
+		position: relative;
+		top: 20px;
+		left: 50%;
+		margin-left: -45%;
+	}
+	.cancel-p{
+		position: relative !important;
+		top: 30px;
+		left: 50% !important;
+		margin: 0 0 0 -10px !important;
+
+	}
+</style>
---
+++ src/pages/index/orderConfirm.vue
@@ -0,0 +1,118 @@
+<template>
+<view class="o-container" style="height:100%">
+    <view class="order-infor" style="height: 24%;box-shadow:0 2px 8px rgb(236, 246, 247)">
+        <view class="v-order">
+            <image src="../../static/img/incon-or.png"></image>
+            <text style="color:#323232;font-size:17px;position: relative;top: 10px;left:10px">{{aera}}</text>
+        </view>
+        <view style="margin-top:25px" class="v-order">
+            <text>角色名称：</text>
+            <text>{{role}}</text>
+        </view>
+        <view style="margin-top:20px" class="v-order">
+            <text>段位：</text>
+            <text>{{strage}}</text>
+        </view>
+    </view>
+    <view class="order-price" style="height: 23%;box-shadow:0 2px 8px rgb(236, 246, 247)">
+        <view class="v-price v-order" >
+            <text>单价(元)</text>
+            <text class="v-text">{{price}}</text>
+        </view>
+        <view class="v-price v-order">
+            <text>盘数</text>
+            <text class="v-text">{{num}}</text>
+        </view>
+        <view class="v-price v-order">
+            <text>总计</text>
+            <text class="v-text" style="color:#EEB357">{{total}}</text>
+        </view>
+    </view>
+    <view class="order-notice" style="height: 38%;box-shadow:0 2px 8px rgb(236, 246, 247)">
+        <view class="v-order">
+            <h5 style="color:#346CD5;margin:10px 0 10px 0;font-weight:900">买家须知</h5>
+        </view>
+        <view class="v-order">
+            <text style="font-size:14px;font-family:'宋体'">
+                陪练过程中，如果当前这盘游戏失败、并且陪练员没有被评定为svp，可以及时截图，并在订单页面申请退款、上传当时的截图，
+                经核查属实，将会退还给您失败盘数的金额。但如果未达到以上条件，本单局不予以退款。
+                如有特殊情况，请至“我的”页面联系客服并描述事实经过，我们会酌情解决。
+            </text>
+        </view>
+    </view>
+    <view class="order-submit" style="position:relative;height: 10%">
+        <view  style="float:left">
+            <text class="total">{{total}}</text>
+            <button class="bt-order">提交订单</button>
+        </view>
+    </view>
+</view>
+</template>
+
+<script>
+    export default{
+       data(){
+           return {
+               aera:'艾欧尼亚 ',
+               role:'骑着',
+               strage:'白银',
+               price:'60.00',
+               num:'10' ,
+               total:'600.00'
+           }
+       } 
+    }
+</script>
+
+<style>
+page{
+	height: 100%;
+    padding: 0;
+    margin: 0;
+	}
+/* .order-infor, .order-price{
+    height: 25%;
+    border:1px solid black;
+} */
+.order-infor image{
+    width: 22px;
+    height: 22px;
+    position: relative;
+    top: 15px;
+}
+.v-order{
+    width: 90%;
+    margin-left: 5%;
+}
+.v-price{
+    margin-top:15px
+}
+/* .order-notice{
+    height: 40%;
+    border:1px solid black;
+}
+.order-submit{
+    height: 10%;
+    border:1px solid black;
+} */
+.v-text{
+    float: right;
+}
+.bt-order{
+    width:40%;
+    height: 100%;
+    background-color: #5C92F6;
+    color: white;
+    border-radius: 0;
+    position: absolute;
+    right: 0;
+    display: inline-block;
+
+}
+.total{
+    color: #EEB357;
+    position: absolute;
+    top: 25%;
+    margin-left: 15px;
+}
+</style>
